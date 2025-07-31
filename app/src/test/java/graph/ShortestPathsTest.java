4cf88ea45ea9d2e9eabd7dda352cf44ac6227c85
Kevon Hyde <hydek@wwu.edu>
2025-07-31 09:59:17 -0700

    was unaware i had to upload tests, sorry

--- app/src/test/java/graph/ShortestPathsTest.java
+++ app/src/test/java/graph/ShortestPathsTest.java
@@ -20,6 +20,7 @@ public class ShortestPathsTest {
     private String getGraphResource(String fileName) {
         ClassLoader classLoader = getClass().getClassLoader();
         URL resource = classLoader.getResource(fileName);
+        System.out.println(resource.getPath());
         return resource.getPath();
     }
 
@@ -64,6 +65,63 @@ public class ShortestPathsTest {
         assertEquals(abPath.getFirst(), a);
         assertEquals(abPath.getLast(),  b);
         assertEquals(sp.shortestPathLength(b), 1.0, 1e-6);
+    }
+
+    @Test
+    public void test02Simple1() {
+        Graph g = loadBasicGraph("Simple1.txt");
+        g.report();
+        ShortestPaths sp = new ShortestPaths();
+        Node a = g.getNode("A");
+        sp.compute(a);
+        Node c = g.getNode("C");
+        Node s = g.getNode("S");
+        LinkedList<Node> acPath = sp.shortestPath(c);
+        LinkedList<Node> asPath = sp.shortestPath(s);
+        //test path from a to c
+        assertEquals(acPath.size(), 2);
+        assertEquals(acPath.get(0), a);
+        assertEquals(acPath.get(1), c);
+        assertEquals(sp.shortestPathLength(c), 2.0, 1e-6);
+        //test path from a to s
+        String[] nodes = {"A", "C", "D", "S"};
+        for (int i = 0; i < nodes.length; i++) {
+            assertEquals(asPath.get(i).toString(), nodes[i]);
+        }
+        assertEquals(sp.shortestPathLength(s), 5.0, 1e-6);
+    }
+
+    @Test
+    public void test03Simple2() {
+        Graph g = loadBasicGraph("Simple2.txt");
+        g.report();
+        ShortestPaths sp = new ShortestPaths();
+        Node d = g.getNode("D");
+        Node c = g.getNode("C");
+        Node ii = g.getNode("I");
+        Node f = g.getNode("F");
+        Node gg = g.getNode("G");
+        //test node d
+        sp.compute(d);
+        LinkedList<Node> diPath = sp.shortestPath(ii);
+        String[] nodes = {"D", "A", "E", "F", "I"};
+        for (int i = 0; i < nodes.length; i++) {
+            assertEquals(diPath.get(i).toString(), nodes[i]);
+        }
+        assertEquals(sp.shortestPathLength(ii), 9.0, 1e-6);
+        assertEquals(sp.getPaths().size(), 10);
+        //test node c
+        sp.compute(c);
+        LinkedList<Node> cfPath = sp.shortestPath(f);
+        assertEquals(sp.shortestPath(f).size(), 0);
+        assertEquals(sp.shortestPathLength(f), Double.POSITIVE_INFINITY, 1e-6);
+        assertEquals(sp.getPaths().size(), 1);
+        //test node f
+        sp.compute(f);
+        assertEquals(sp.getPaths().size(), 6);
+        LinkedList<Node> ffPath = sp.shortestPath(f);
+        assertEquals(ffPath.getFirst().toString(), "F");
+        assertEquals(ffPath.getLast().toString(), "F");
     }
 
     /* Pro tip: unless you include @Test on the line above your method header,
